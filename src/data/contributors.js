038dc5d294e171f38e9b483bd157d064d1716f42
jongmal <zotnlnn@gmail.com>
2021-03-21 17:01:37 +0900

    edit

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -6,12 +6,7 @@ const contributors = {
       name: '도돌',
       email: 'zotnlnn@gmail.com',
       description: `
-        새로운 것에 도전하는 것을 즐기는 개발자입니다. 
-        실리콘밸리에서 있었던 시간들을 재미있게 이야기 해볼 방법이 없을까 🤔 
-        하다 친구들을 꼬셔서 프로젝트를 시작했습니다. 
-        평소에 flutter로 앱 개발을 하던 개발자가, 
-        갑자기 React를 배워서 혼자서 3주 만에 뚝딱뚝딱 웹을 개발하게 되는데..! 
-        (두둥! 🤭)
+        우리집 고양이 자랑하려다가 만들게 된 테스트 페이지 : )
       `,
       link: [
         {
@@ -22,7 +17,7 @@ const contributors = {
         {
           id: 2,
           title: 'Blog',
-          url: 'https://naraekn.github.io/',
+          url: 'https://jongmal.github.io/',
         },
       ],
     },
